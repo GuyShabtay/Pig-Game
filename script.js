60b0a3a0bf14514caf9d508c039d3a8dd1e76747
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-22 17:27:08 +0300

    selceting the buttons

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ const score1El=document.querySelector('#score--1')
 //the same thing
 //const score0El=document.getElementById('score--0')
 const diceEl=document.querySelector('.dice');
+const btnNew=document.querySelector('.btn--new');
+const btnRoll=document.querySelector('.btn--roll');
+const btnHold=document.querySelector('.btn--hold');
 
-
+//starting conditions
 score0El.textContent=0;
 score1El.textContent=0;
 diceEl.classList.add('hidden');
 
+//
