1a3476c80cbd78a63c3b60ec360dd24147d7faa5
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-22 17:15:10 +0300

    hiding the dice

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const score0El=document.querySelector('#score--0')
 const score1El=document.querySelector('#score--1')
 //the same thing
 //const score0El=document.getElementById('score--0')
+const diceEl=document.querySelector('.dice');
+
 
 score0El.textContent=0;
 score1El.textContent=0;
+diceEl.classList.add('hidden');
+
