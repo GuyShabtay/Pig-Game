de4b98919bb4c7ec4c2f5cf0c14ad2222bf61ea0
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-22 17:11:27 +0300

    reset start score

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+//selcting elements
+const score0El=document.querySelector('#score--0')
+const score1El=document.querySelector('#score--1')
+//the same thing
+//const score0El=document.getElementById('score--0')
+
+score0El.textContent=0;
+score1El.textContent=0;
