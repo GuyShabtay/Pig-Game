6110d9f3751953806d9cd6cc504b4c5e667f099a
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-22 19:42:53 +0300

    winning the game

--- script.js
+++ script.js
@@ -21,9 +21,22 @@ diceEl.classList.add('hidden');
 const scores=[0,0];
 let currentScore=0;
 let activePlayer=0;
+let playing=true;
+
+const switchPlayer=function(){
+  document.querySelector(`#current--${activePlayer}`).textContent=0;
+    currentScore=0;
+    activePlayer=activePlayer===0 ? activePlayer=1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+
+};
 
 //rolling the dice functuallity
 btnRoll.addEventListener('click',function(){
+  if (playing){
+
+  
   //1. generating a random dice roll
   const dice=Math.trunc(Math.random()*6)+1;
   //2.display the dice
@@ -36,10 +49,26 @@ btnRoll.addEventListener('click',function(){
     document.querySelector(`#current--${activePlayer}`).textContent=currentScore;
   }else{
     //switch to next player
-    document.querySelector(`#current--${activePlayer}`).textContent=0;
-    currentScore=0;
-    activePlayer=activePlayer===0 ? activePlayer=1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
+    
+  }}
+});
+
+btnHold.addEventListener('click',function(){
+  if (playing){
+  //add current score to active player's score
+  scores[activePlayer]+=currentScore;
+  document.querySelector(`#score--${activePlayer}`).textContent=scores[activePlayer];
+  //check if player's score>=100
+  if (scores[activePlayer]>=10){
+    //finish the game
+    playing=false;
+    diceEl.classList.add('hidden');
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+  }else{
+  //switch to the next player
+  switchPlayer();
+  }
   }
 });
