15f86a75b342cd4886560385e941eee773b26b20
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-23 02:24:39 +0300

    reset the score

--- script.js
+++ script.js
@@ -12,16 +12,34 @@ const diceEl=document.querySelector('.dice');
 const btnNew=document.querySelector('.btn--new');
 const btnRoll=document.querySelector('.btn--roll');
 const btnHold=document.querySelector('.btn--hold');
+const name0El=document.querySelector('#name--0');
+const name1El=document.querySelector('#name--1');
+
+let scores,currentScore,activePlayer,playing;
 
 //starting conditions
-score0El.textContent=0;
-score1El.textContent=0;
-diceEl.classList.add('hidden');
+const init=function(){
 
-const scores=[0,0];
-let currentScore=0;
-let activePlayer=0;
-let playing=true;
+  
+  scores=[0,0];
+  currentScore=0;
+  activePlayer=0;
+  playing=true;
+  
+  score0El.textContent=0;
+  score1El.textContent=0;
+  current0El.textContent=0;
+  current1El.textContent=0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  document.querySelector('#name--0').textContent='PLAYER 1';
+  document.querySelector('#name--1').textContent='PLAYER 2';
+
+};
+init();
 
 const switchPlayer=function(){
   document.querySelector(`#current--${activePlayer}`).textContent=0;
@@ -49,7 +67,11 @@ btnRoll.addEventListener('click',function(){
     document.querySelector(`#current--${activePlayer}`).textContent=currentScore;
   }else{
     //switch to next player
+    scores[activePlayer]=0;
+    document.querySelector(`#score--${activePlayer}`).textContent=scores[activePlayer];
+
     switchPlayer();
+
     
   }}
 });
@@ -60,15 +82,17 @@ btnHold.addEventListener('click',function(){
   scores[activePlayer]+=currentScore;
   document.querySelector(`#score--${activePlayer}`).textContent=scores[activePlayer];
   //check if player's score>=100
-  if (scores[activePlayer]>=10){
+  if (scores[activePlayer]>=80){
     //finish the game
     playing=false;
     diceEl.classList.add('hidden');
     document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
     document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    document.querySelector(`#name--${activePlayer}`).textContent=`PLAYER ${activePlayer} WINS !`;
   }else{
   //switch to the next player
   switchPlayer();
   }
   }
 });
+btnNew.addEventListener('click',init);
